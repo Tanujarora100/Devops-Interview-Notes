d4da6775cf2fb01869aaef7c679e9bbf6c8abf78
Tanuj Arora <tanujarora@Tanujs-MacBook-Air.local>
2024-08-13 10:44:51 +0530

    service status

--- Scripting/Intermediate/Service-Status.sh
+++ Scripting/Intermediate/Service-Status.sh
@@ -3,6 +3,6 @@ Service= "nginx"
 SERVICE_LOG= "/var/log/nginx"
 CURRENT_DATE= $(date +%Y-%m-%d-%H:%M:%S)
 if ! systemctl is-active "$Service" --quiet; then 
-    echo "Service is not active at CURRENT_DATE "  >> "$SERVICE_LOG"
+    echo "Service is not active at $CURRENT_DATE "  >> "$SERVICE_LOG"
 else 
     echo "Service is active at $CURRENT_DATE " >> "$SERVICE_LOG" 
