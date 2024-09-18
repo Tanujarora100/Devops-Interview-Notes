4688f2077207a092959dd1667c3d62e4374fba34
Tanuj Arora <tanujarora@Tanujs-MacBook-Air.local>
2024-09-18 14:18:09 +0530

    Scripting added

--- Scripting/ShellScripts/ImportantScripts/RemoveBlankLines.sh
+++ Scripting/ShellScripts/ImportantScripts/RemoveBlankLines.sh
@@ -1,10 +1,12 @@
 #!/bin/bash 
-FILE_PATH="/var/log"
+set -e 
+set -x 
+FILE_PATH="/var/log/system.log"
 if [[ ! -f "$FILE_PATH" ]]; then 
     echo " Invalid File Path Provided" >> /dev/null 2>&1
     exit 1
 fi
-NEW_FILE="/users/$USER/withoutblanks.txt" 
+NEW_FILE="$PWD/withoutblanks.txt" 
 > "$NEW_FILE"
 awk 'NF' "$FILE_PATH" >> "$NEW_FILE"
 echo "File processed successfully. New file created at $NEW_FILE"
